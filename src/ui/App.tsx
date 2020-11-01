babfdfba3a1449ada933ea15c4190706e4aa13c6
laurawarren345 <lc.warren2@gmail.com>
2020-11-01 17:22:13 +0000

    Set up and create grid square

---
+++ src/ui/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import EmptySquare from './EmptySquare'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <EmptySquare />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ui/EmptySquare.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./square.module.css";
+
+export default function EmptySquare() {
+  return (
+    <div className={style.emptySquare}></div>   
+  )
+}
